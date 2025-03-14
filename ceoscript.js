38318d7b30c0917374d8e1ae0dd8149a797223ba
CE-0 <93526532+CE-0@users.noreply.github.com>
2025-03-14 01:53:40 +0000

    Added page of links to my various accounts

--- ceoscript.js
+++ ceoscript.js
@@ -6,7 +6,11 @@ const blogTxt = '';
 
 const projTxt = '{"projects":[{"name":"Tall Order", "desc":"My first Godot game. Can you catch all the falling ingredients? Just be sure not to get anything rotten in the mix.", "image":"imgs/project_imgs/Tall Order.png", "link":"https://github.com/CE-0/Tall-Order"}]}';
 
+const linkTxt = '{"links": [{"name":"Github", "image":"imgs/link_imgs/GitHub logo.jpg", "url":"https://github.com/CE-0"}, {"name":"itch.io", "image":"imgs/link_imgs/Itch.io logo.png", "url":"https://ce-0.itch.io/"}]'
+
 const jsonProjects = JSON.parse(projTxt); 
+
+const jsonLinks = JSON.parse(linkTxt);
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
@@ -14,8 +18,11 @@ function getRandomInt(max) {
 					 
 document.getElementById("header").innerHTML = headerArray[getRandomInt(headerArray.length)]; 
 
-let c = 0;
-
 for (let i = 0; i < jsonProjects.projects.length; i++) {
 	document.getElementsByClassName("projectList")[0].innerHTML = `<h2>${jsonProjects.projects[i].name}</h2> <img src="${jsonProjects.projects[i].image}" width="400" height="400"> <p class="sitetext">${jsonProjects.projects[i].desc}</p> <a href="${jsonProjects.projects[i].link}" target="_blank">Find it here</a> <hr>`
-}
+} 
+
+for (let i = 0; i < jsonLinks.links.length; i++) {
+	document.getElementsByClassName("linkList")[0].innerHTML = `<a href="${jsonLinks.links[i].url}" target="_blank"><img src="${jsonLinks.links[i].image}"></a> <p class="sitetext">${jsonLinks.links[i].name}</p> <hr>`
+} 
+
