51629f69b5950c86b00d94f402c72d307c01b54c
CE-0 <93526532+CE-0@users.noreply.github.com>
2025-12-11 00:36:35 +0000

    Updated skill and project lists

--- ceoscript.js
+++ ceoscript.js
@@ -1,10 +1,10 @@
 const headerArray = ["Wipe your feet before entering", "Viewer discretion is advised", "Don't let the door hit you on the way in",
-					"Winner of the esteemed Gold X Award in 2004", "5 out of 10 dentists recommend", "Please keep your hands and feet in the ride at all times",
+					"Winner of the esteemed Gold X Award in 2004", "Infinity out of 10 dentists recommend", "Please keep your hands and feet in the ride at all times",
 					"State your business immediately", "This message will self destruct in 2 seconds", 
 					"MESSAGE REDACTED FOR EXPLICIT CONTENT", "If you see all these messages, you'll be rewarded handsomely"]; 
 const blogTxt = '';
 
-const projTxt = '{"projects":[{"name":"Tall Order", "desc":"My first Godot game. Can you catch all the falling ingredients? Just be sure not to get anything rotten in the mix.", "image":"imgs/project_imgs/Tall Order.png", "link":"https://github.com/CE-0/Tall-Order", "id":"tall-order"}, {"name":"Grave Danger", "desc":"A submission to the Pixel Game Jam (May 11 - May 20) that I developed in a team. Grow as many plants as you can in this garden that hides a deadly secret.", "image":"imgs/project_imgs/Grave Danger.png", "link":"https://the-shelley.itch.io/grave-danger", "id":"grave-danger"}, {"name":"Wave Mage", "desc":"A submission to the 2025 Game Off (November 1 - December 1) that I developed as part of a team. As a medieval wizard, use and manipulate the power of sound waves to ward off vicious monsters.", "image":"imgs/project_imgs/Wave Mage.png", "link":"https://pancholope321.itch.io/wave-mage", "id":"wave-mage"}]}';
+const projTxt = '{"projects":[{"name":"Tall Order", "desc":"My first Godot game. Can you catch all the falling ingredients? Just be sure not to get anything rotten in the mix.;I was the sole developer of this project, handling the programming, art and sound effects.", "tools":["Godot", "GDScript"], "image":"imgs/project_imgs/Tall Order.png", "link":"https://github.com/CE-0/Tall-Order", "id":"tall-order"}, {"name":"Grave Danger", "desc":"A submission to the Pixel Game Jam (May 11 - May 20) that I developed in a team. Grow as many plants as you can in this garden that hides a deadly secret.", "tools":["Godot", "GDScript"], "image":"imgs/project_imgs/Grave Danger.png", "link":"https://the-shelley.itch.io/grave-danger", "id":"grave-danger"}, {"name":"Wave Mage", "desc":"A submission to the 2025 Game Off (November 1 - December 1) that I developed as part of a team. As a medieval wizard, use and manipulate the power of sound waves to ward off vicious monsters.", "tools":["Godot", "GDScript", "JSON"], "image":"imgs/project_imgs/Wave Mage.png", "link":"https://pancholope321.itch.io/wave-mage", "id":"wave-mage"}]}';
 
 //const projTxt = '{"projects":[{"name":"Tall Order", "desc":"My first Godot game. Can you catch all the falling ingredients? Just be sure not to get anything rotten in the mix.", "image":"imgs/project_imgs/Tall Order.png", "link":"https://github.com/CE-0/Tall-Order", "id":"tall-order"}, {"name":"Grave Danger", "desc":"A submission to the Pixel Game Jam (May 11 - May 20) that I developed in a team. Grow as many plants as you can in this garden that hides a deadly secret.", "image":"imgs/project_imgs/Grave Danger.png", "link":"https://the-shelley.itch.io/grave-danger", "id":"grave-danger"}]}';
 
@@ -25,7 +25,20 @@ function loadList() {
 	switch (page) {
 		case "projects.html":
 			for (let i = 0; i < jsonProjects.projects.length; i++) {
-				document.getElementsByClassName("projectList")[0].innerHTML += `<h2 id="${jsonProjects.projects[i].id}">${jsonProjects.projects[i].name}</h2> <img src="${jsonProjects.projects[i].image}" width="400" height="400"> <p class="sitetext">${jsonProjects.projects[i].desc}</p> <a href="${jsonProjects.projects[i].link}" target="_blank">Find it here</a> <hr>`;
+				let dsc = ""; 
+				let tools = `<p class="sitetext">Tools: `;
+				let dscArr = jsonProjects.projects[i].desc.split(";");
+				for (let j = 0; j < dscArr.length; j++) {
+					dsc += `<p class="sitetext">${dscArr[j]}</p>`
+				}
+				for (let k = 0; k < jsonProjects.projects[i].tools.length; k++) {
+					tools += `${jsonProjects.projects[i].tools[k]}` 
+					if (k != jsonProjects.projects[i].tools.length-1) {
+						tools += `, `
+					}
+				}
+				tools += `</p>`
+				document.getElementsByClassName("projectList")[0].innerHTML += `<h2 id="${jsonProjects.projects[i].id}">${jsonProjects.projects[i].name}</h2> <img src="${jsonProjects.projects[i].image}" width="400" height="400"> ${dsc} <br> ${tools} <br> <a href="${jsonProjects.projects[i].link}" target="_blank">Find it here</a> <hr>`;
 			} 
 		break;
 		
